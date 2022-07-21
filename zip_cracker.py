2efa0876ab7b7abd33e513802c7ae700b1205376
xVjVx <jaimedsm127@gmail.com>
2022-07-21 05:57:57 -0500

    Add zip_cracker

---
+++ zip_cracker.py
@@ -0,0 +1,39 @@
+from pathlib import Path
+import zipfile
+import sys
+import os
+
+if len(sys.argv) != 3:
+    print("[!] Correct execution: python3 %s [zipfile] [wordlist]"%(sys.argv[0]))
+    sys.exit(0)
+
+def extractZip(zipFile, password):
+    try:
+        zipFile.extractall(pwd=bytes(password, 'utf-8'))
+        return password
+    except:
+        return
+
+def main():
+    zipFile = sys.argv[1]
+    wordlist = sys.argv[2]
+    textFileVerification = os.path.splitext(wordlist)[-1].lower()
+    zipVerification = zipfile.is_zipfile(zipFile)
+    if zipVerification == True:
+        zipFile = zipfile.ZipFile(sys.argv[1])
+    else:
+        print("[!] Write an existing Zip file")
+    if textFileVerification == ".txt":
+            textFileVerification = wordlist
+    else:
+        print("[!] Write an existing text file")
+    wordlist = open(wordlist) 
+    for line in wordlist.readlines():
+        password = line.strip("\n")
+        crack = extractZip(zipFile, password)
+        if crack:
+            print("[!] The password is: " + password)
+            break
+
+if __name__ == '__main__':
+    main()
